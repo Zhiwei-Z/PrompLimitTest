51bcb47882a3873de49a8157f3d29ae3e461c04d
Zhiwei-Z <zhiweizhang2012@gmail.com>
2019-12-03 00:12:04 -0800

    3 pos 1 neg ho pos

--- meta_policy_search/envs/mujoco_envs/half_cheetah_rand_vel.py
+++ meta_policy_search/envs/mujoco_envs/half_cheetah_rand_vel.py
@@ -19,9 +19,9 @@ class HalfCheetahRandVelEnv(MetaEnv, MujocoEnv, gym.utils.EzPickle):
         task = TASKSL1
         task = np.append(task, [-0.1])
         if out_disabled:
-            return np.full(n_tasks, -0.5)
+            # return np.full(n_tasks, -0.5)
 
-            # return np.full(n_tasks, task[len(task) - 1] + 0.5)
+            return np.full(n_tasks, task[len(task) - 1] + 0.5)
         return np.array([task[idx] for idx in np.random.choice(range(len(task)), size=n_tasks)])
 
     def set_task(self, task):
